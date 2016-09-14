5b951475d0020c990ea59ea3113d8f87fed6ed51
Deka Auliya <deka108@gmail.com>
2016-09-15 00:46:11 +0800

    Update json with answer_count

--- filter.py
+++ filter.py
@@ -1,4 +1,3 @@
-# filter = !17vhT8QOUm)koH5(u-VKOO052pl0CqLsE)qJhVj8Pg-YDP
 import os, time
 import requests
 import json
@@ -8,44 +7,47 @@ os.environ['TZ']='UTC'
 
 # Filtering options
 pagesize = 100
-filter = '!17vhT8QOUm)koH5(u-VKOO052pl0CqLsE)qJhVj8Pg-YDP'
+# filter w/o answer count = !17vhT8QOUm)koH5(u-VKOO052pl0CqLsE)qJhVj8Pg-YDP
+filter = '!)EhwLl5mQ7SRRT.giqkyY*Ml6ZLhMFIWPHEQ-OGv88TMTWrEL' # with count
 order = 'desc'
 sort = 'votes'
 site = 'stackoverflow'
 
-# Converts date into epoch 
+# Date formats
 cur_year = 2016
 from_date = '%s-01-01'
 to_date = '%s-12-31'
 date_format = '%Y-%m-%d'
 
-# Initial URL
+# Initial URL + payload
 url = 'https://api.stackexchange.com/2.2/questions'
 payload = {'pagesize': pagesize, 'order': order, 'sort': sort, 'site': site,
 'filter': filter}
 
 def update_year(year):
-	"""Change the from and to date using UTC epoch"""
+	"""Updates the from and to date in UTC epoch."""
 	payload['fromdate'] = int(time.mktime(time.strptime(from_date % year, date_format)))
 	payload['todate'] = int(time.mktime(time.strptime(to_date % year, date_format)))
 
 def generate_json(year):
-	"""Attempts to generate JSON file from 2007-2016."""
-	while(year >= 2007):
+	"""Attempts to generate JSON file from 2008-2016."""
+	while(year >= 2008):
 		update_year(year)
 
-		# Sends a GET request to stackexchange API.
+		# Sends a GET request via StackExchange API.
 		# Example: https://api.stackexchange.com/2.2/questions?pagesize=100&fromdate=1356998400&todate=1388448000&order=desc&sort=votes&site=stackoverflow&filter=!17vhT8QOUm)koH5(u-VKOO052pl0CqLsE)qJhVj8Pg-YDP
 		res = requests.get(url, params=payload)
 
-		# Write json result into a json file
+		# Write JSON result into a JSON file
 		file_name = 'data_' + str(year) + '.json'
-		with open(file_name, 'w') as output:
+		path = os.path.join("data", file_name)
+		with open(path, 'wb') as output:
 			json.dump(res.json(), output)
+		
 		year -= 1
 
 def check_questions(year):
-	while(year >= 2007):
+	while(year >= 2008):
 		file_name = 'data_' + str(year) + '.json'
 		with open(file_name) as input:
 			data = json.load(input)
@@ -54,6 +56,6 @@ def check_questions(year):
 		year -= 1
 		
 
-#generate_json(2016)
-check_questions(2016)
+generate_json(2016)
+#check_questions(2016)
 
