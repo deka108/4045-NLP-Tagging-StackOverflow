4dbd46b1d0cd65906ce2eab2400ccad2987b5df9
Peter <peterch1ang@yahoo.com>
2016-10-10 17:31:28 +0800

    fix init

--- stanford-ner/init.bat
+++ stanford-ner/init.bat
@@ -1,1 +1,1 @@
-set CLASSPATH=stanford-ner.jar;lib\joda-time.jar;lib\jollyday-0.4.7.jar;lib\slf4j-api.jar;lib\slf4j-simple.jar;lib\stanford-ner-resources.jar;
+set CLASSPATH=%~dp0stanford-ner.jar;%~dp0lib\joda-time.jar;%~dp0lib\jollyday-0.4.7.jar;%~dp0lib\slf4j-api.jar;%~dp0lib\slf4j-simple.jar;%~dp0lib\stanford-ner-resources.jar;
