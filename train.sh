828cb59739737edfe13a27fdf4c66d704af08396
deka108 <deka108@gmail.com>
2016-10-23 07:53:29 +0800

    Training with different random training-test split.

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 export CLASSPATH=$(find "./stanford-ner" -name '*.jar' | xargs echo | tr ' ' ':')
 for i in {0..3}
 do
-	java edu.stanford.nlp.ie.crf.CRFClassifier -prop train/train-$i.prop
+	java edu.stanford.nlp.ie.crf.CRFClassifier -prop train/train-$i.prop 2> train/train-log-$i.txt
 done
