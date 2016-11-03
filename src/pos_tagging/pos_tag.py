fc723cdb21120b64bbb0a0e0637fceafb2f4c04d
deka108 <deka108@gmail.com>
2016-11-04 04:26:44 +0800

    Move folders for submission

--- src/pos_tagging/pos_tag.py
+++
@@ -1,7 +0,0 @@
-import nltk
-
-with open("sentence.txt", "r") as source, open("result.txt", "a") as target:
- 	for line in source:
- 		text = nltk.word_tokenize(line)
- 		target.write( "%s\n" % nltk.pos_tag(text) )
-
--- src/test.bat
+++
@@ -1,6 +0,0 @@
-SETLOCAL
-CALL stanford-ner\init.bat
-FOR /L %%i IN (0,1,3) DO (
-    java edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier train/api_mention-%%i.ser.gz -testFile train/test-%%i.tsv 1> train/result-%%i.tsv 2> train/score-%%i.txt
-)
-ENDLOCAL
